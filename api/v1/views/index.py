3538526aaad0a37e21daae716811825e1a9af4a1
LiantsoaANDR <liantsoa.cpro@gmail.com>
2024-05-01 13:04:36 -0700

    Task 4

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,6 +4,13 @@
 from api.v1.views import app_views
 from flask import jsonify
 from models import storage
+from models import storage
+from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
 
 
 @app_views.route('/status', methods=['GET'])
