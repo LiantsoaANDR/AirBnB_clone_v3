f70f5acdfc087e0ace722130935fb49ff113793e
LiantsoaANDR <liantsoa.cpro@gmail.com>
2024-05-01 13:16:29 -0700

    Task 4

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,12 +4,6 @@
 from api.v1.views import app_views
 from flask import jsonify
 from models import storage
-from models.user import User
-from models.place import Place
-from models.state import State
-from models.city import City
-from models.amenity import Amenity
-from models.review import Review
 
 
 @app_views.route('/status', methods=['GET'])
@@ -18,7 +12,7 @@ def status():
     return jsonify({"status": "OK"})
 
 
-@app_views.route('/status', methods=['GET'])
+@app_views.route('/stats', methods=['GET'])
 def get_stats():
     """Retrieves the number of each object by type"""
 
