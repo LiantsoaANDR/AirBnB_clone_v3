aa7303d7dc421fea209af16cf5f31695ec8269d6
LiantsoaANDR <liantsoa.cpro@gmail.com>
2024-05-01 12:19:00 -0700

    Task 4

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -7,5 +7,14 @@ from flask import jsonify
 
 @app_views.route('/status', methods=['GET'])
 def status():
-    """Returns API status"""
-    return jsonify({"status": "OK"})
+    """Retrieves the number of each object by type"""
+
+    counts = {
+        'amenities': storage.count('Amenity'),
+        'cities': storage.count('City'),
+        'places': storage.count('Place'),
+        'reviews': storage.count('Review'),
+        'states': storage.count('State'),
+        'users': storage.count('User')
+    }
+    return jsonify(counts)
